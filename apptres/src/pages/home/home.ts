07b13c33db57c68207f14e882aa9f3deb75ce37f
ESTUDIANTE <hygros10@gmail.com>
2018-11-01 20:26:27 -0500

    nueva app tamb instalacion de componentes

---
+++ apptres/src/pages/home/home.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { NavController, ActionSheetController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,
+    public actionSheetCtrl: ActionSheetController
+    ) {
+
+  }
+  presentActionSheet() {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Mi prototipo coysa',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+}
